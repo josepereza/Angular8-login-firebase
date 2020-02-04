114fff3bb976e5077f5eb0d24754f17a1bab8def
Angel Paz <angelrpaz54@gmail.com>
2020-02-03 23:18:12 -0600

    Creamos Servicio Auth y el Registro

--- src/app/componentes/register-page/register-page.component.ts
+++ src/app/componentes/register-page/register-page.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
 
