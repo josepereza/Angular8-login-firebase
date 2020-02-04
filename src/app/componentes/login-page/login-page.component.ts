6b93fd4106248dcedbe11451dec296475e826954
Angel Paz <angelrpaz54@gmail.com>
2020-02-04 10:32:13 -0600

    Añadimos Login y Logout

--- src/app/componentes/login-page/login-page.component.ts
+++ src/app/componentes/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -8,10 +9,31 @@ import { Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  public email: string;
+  public password: string;
+
+  constructor(public authService: AuthService,
+              public router: Router) { }
 
   ngOnInit() {
   }
+
+  onSubmitLogin() {
+   this.authService.loginEmail(this.email, this.password)
+   .then( (resp) => {
+     // this.flashMensaje.show('Usuario logado correctamente.',
+     // {cssClass: 'alert-success', timeout: 4000});
+     console.log("Ingreso exitoso")
+     console.log(resp)
+     this.router.navigate(['/privado']);
+   }).catch((err) => {
+     // this.flashMensaje.show(err.message,
+     // {cssClass: 'alert-danger', timeout: 4000});
+     console.log("Fallo")
+     console.log(err)
+     this.router.navigate(['/login']);
+   });
+ }
 
   onLoginFB(){
 
